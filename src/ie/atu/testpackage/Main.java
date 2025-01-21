d82689226eadce8613c51fd3152671b41a431c23
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-01-21 23:16:03 +0000

    Updates

--- src/ie/atu/testpackage/Main.java
+++ src/ie/atu/testpackage/Main.java
@@ -1,7 +1,6 @@
 package ie.atu.testpackage;
 
-// This program prints 'Hello, World!' to the console.
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         // Prints 'Hello, World!' to the console.
         System.out.println("Hello, World!");
